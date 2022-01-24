03148a0fd052d75f353a1c425ba4a606dfc17bf0
Justin Bane <justbane@gmail.com>
2022-01-23 16:51:19 -0800

    error

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h1>Hello World!!! deployed</h1>>
+          <h1>Hello World!!! deployed</h1>
         </a>
       </header>
     </div>
